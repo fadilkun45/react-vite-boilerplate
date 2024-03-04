5bf9d6ce12104867bd8a30ac6e06a6288592ed74
fadilkun45 <indocrafter76@gmail.com>
2024-03-04 11:34:05 +0700

    update: move chakraProvider to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
 import { Welcome } from './Pages/Welcome'
 
 function App() {
 
   return (
-   <ChakraProvider>
      <BrowserRouter>
       <Routes>
         <Route path='/' Component={Welcome} />
       </Routes>
     </BrowserRouter>
-   </ChakraProvider>
   )
 }
 
